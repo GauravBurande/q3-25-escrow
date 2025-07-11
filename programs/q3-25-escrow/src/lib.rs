30dcddb5c83d0b0926f7243f09458c9c1c14ad3f
Gaurav <burandegaurav8899@gmail.com>
2025-07-11 12:35:16 +0530

    feat: refund instruction and lib file done

--- programs/q3-25-escrow/src/lib.rs
+++ programs/q3-25-escrow/src/lib.rs
@@ -15,7 +15,22 @@ declare_id!("6ynKAn3rXKKXAqoAP5ik8TqV91swMSjsQqHVd1vER9oV");
 pub mod q3_25_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(ctx: Context<Make>, seed: u64, amount: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(receive, seed, &ctx.bumps)?;
+        ctx.accounts.deposit(amount)?;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()?;
+
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.transfer_and_close_vault()?;
+
+        Ok(())
     }
 }
